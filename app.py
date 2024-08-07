217214e4428dee19d5fdafa5afe1d7e32305119e
ibraheematradies <atifkhan4146@gmail.com>
2024-08-07 07:50:19 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by zero."
+    return x / y
+
+st.title("Ibraheem's Calculator")
+
+num1 = st.number_input("Enter the first number:", format="%f")
+num2 = st.number_input("Enter the second number:", format="%f")
+
+operation = st.selectbox("Select operation", ["Add", "Subtract", "Multiply", "Divide"])
+
+if st.button("Calculate"):
+    if operation == "Add":
+        result = add(num1, num2)
+    elif operation == "Subtract":
+        result = subtract(num1, num2)
+    elif operation == "Multiply":
+        result = multiply(num1, num2)
+    elif operation == "Divide":
+        result = divide(num1, num2)
+
+    st.write(f"The result is: {result}")
